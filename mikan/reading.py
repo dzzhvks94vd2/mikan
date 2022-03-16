f9319ae84898bb50b04f5dd9073059baa24999c9
dzzhvks94vd2 <dzzhvks94vd2@protonmail.com>
2022-03-16 17:33:17 +0100

    Fixed weird import

--- mikan/reading.py
+++ mikan/reading.py
@@ -1,11 +1,11 @@
 from mikan.writing import Writing
-import mikan.utils as utils
+from mikan.utils import is_kana
 
 __all__ = ['Reading']
 
 class Reading(Writing):
 
     def __new__(cls, string: str) -> 'Reading':
-        if not utils.is_kana(string):
+        if not is_kana(string):
             raise ValueError
         return super().__new__(cls, string)
